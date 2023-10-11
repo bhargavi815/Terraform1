daf904a3dad07db0ef1e01aafcf3267c6036b1aa
bhargavi815 <bhargavir815@gmail.com>
2023-10-11 10:47:05 +0530

    my first commit

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ secret_key = "B6v0RKBmySwb9S1UuSF90B0BB4aZsfo2CqD19OAE"
 }
 resource "aws_instance" "example" {
 ami       = "ami-0989fb15ce71ba39e"
-instance_type = "t3.small"
+instance_type = "t3.micro"
 key_name      = "terraform"
 tags          = {
 Name          = "bharu"
